6d7b5426d220175f50494d5b9667255986bd4d3a
jesselynevy <jesselynva@gmail.com>
2025-03-11 18:41:31 +0700

    animation on character

--- homepage.js
+++ homepage.js
@@ -26,15 +26,46 @@ const characters = [
     './assets/finn.png',
     './assets/jake.png',
     './assets/princess_bubblegum.png',
-    './assets/marcelline.webp',
-    './assets/iceking.webp',
+    './assets/marceline.png',
+    './assets/iceking.png',
 ];
+
+const charAnimate = {
+    0: ["./assets/fin-walk-left.png", "./assets/fin-walk-right.png"],
+    1: ["./assets/jake-walk-left.png", "./assets/jake-walk-right.png"],
+    2: ["./assets/princess_bubblegum_walk.png"],
+    3: ["./assets/marceline_walk_left.png", "./assets/marceline_walk_right.png"],
+    4: ["./assets/iceking_walk.png"],
+};
+
 let currentCharIndex = 0;
+let walkInterval;
 //to update character
 function updateCharacter(){
     const characterImg = document.getElementById('character-img');
     characterImg.src = characters[currentCharIndex];
+    moveChar();
 }
+
+function moveChar() {
+    const characterImg = document.getElementById("character-img");
+    const images = charAnimate[currentCharIndex];
+
+    if (walkInterval) clearInterval(walkInterval); 
+
+    if (images.length === 1) {
+        characterImg.src = images[0];
+        characterImg.classList.add("animate-float");
+    } else {
+        characterImg.classList.remove("animate-float");
+        let frame = 0;
+        walkInterval = setInterval(() => {
+            characterImg.src = images[frame];
+            frame = (frame + 1) % images.length;
+        }, 300);
+    }
+}
+
 //previous character
 document.getElementById('prev-char').addEventListener('click', ()=>{
     currentCharIndex = (currentCharIndex-1 + characters.length)%characters.length;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ tailwind.config = {
             },
             animation: {
                 float: "float 1.5s ease-in-out infinite",
+                walk: "walk 0.5s steps(2) infinite",
             },
             keyframes: {
                 float: {
                     "0%, 100%": {transform: "translateY(0)"},
                     "50%": {transform: "translateY(-10px)"},
+                },
+                walk: {
+                    "0%, 100%": { transform: "translateX(0)" },
+                    "50%": { transform: "translateX(2px)" } 
                 }
             }
         }
